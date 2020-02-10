0ae5925b5209ef959e117c5fbd3fdcc7d041ed4c
LoesJanssen <loesjanssen89@gmail.com>
2020-02-10 11:56:54 +0100

    o

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const db = require("./db");
-
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 4000;
 
+const db = require("./db.js");
+const Image = require("./image/model.js");
 app.listen(port, console.log(`Listening on ${port}`));
