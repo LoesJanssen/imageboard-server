019ad06ed41491ade125ce24e755c5d7d8755cd6
LoesJanssen <loesjanssen89@gmail.com>
2020-02-10 14:08:51 +0100

    router.js, index.js post image

--- image/router.js
+++ image/router.js
@@ -3,6 +3,14 @@ const Image = require("./model");
 
 const router = new Router();
 
+router.post("/image", async function(request, response, next) {
+  try {
+    const image = await Image.create(request.body);
+    response.send(image);
+  } catch (error) {
+    next(error);
+  }
+});
 router.get("/image", async (request, response, next) => {
   try {
     const images = await Image.findAll();
--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 4000;
 const imageRouter = require("./image/router");
 
+const bodyParser = require("body-parser");
+
 const db = require("./db.js");
 const Image = require("./image/model.js");
 
+const corsMiddleware = cors();
+app.use(corsMiddleware);
+
+const parserMiddleware = bodyParser.json();
+app.use(parserMiddleware);
+
 app.use(imageRouter);
+
+app.get("/", (req, res) => res.send("Buenas Dias! La pagina della casa"));
 
 app.listen(port, console.log(`Listening on ${port}`));
